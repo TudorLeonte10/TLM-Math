fe39d828f4b6fc661470efeb00a8f8b7d9285032
Mihai Maciuc <Mihai.Maciuc@endava.com>
2025-07-17 16:47:05 +0300

    terminat partea de roluri

--- app/models/operation.py
+++ app/models/operation.py
@@ -7,3 +7,10 @@ class Operation(db.Model):
     input_data = db.Column(db.String(100), nullable=False)    
     result = db.Column(db.String(100), nullable=False)        
     timestamp = db.Column(db.DateTime, default=datetime.utcnow)
+
+class Users(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), nullable=False)
+    password = db.Column(db.String(120), nullable=False)
+    email = db.Column(db.String(120), nullable=False)
+    role = db.Column(db.String(20), default='user')  # Default role is 'user'
--- app/routes/auth_routes.py
+++ app/routes/auth_routes.py
@@ -1,27 +1,27 @@
 from flask import Blueprint, request, jsonify
 import jwt
 from datetime import datetime, timedelta
+from app.models.operation import Users
 
 auth_bp = Blueprint('auth_bp', __name__)
 SECRET_KEY = "supersecret" 
-
 #trebuie mutat in env daca facem cu docker
+
 
 @auth_bp.route('/login', methods=['POST'])
 def login():
     data = request.get_json()
     username = data.get('username')
     password = data.get('password')
-    if username == "admin" and password == "admin123":
-        role = "admin"
-    elif username == "user" and password == "user123":
-        role = "user"
-    else:
+
+    user = Users.query.filter_by(username=username).first()
+
+    if not user or user.password != password:
         return jsonify({"message": "Invalid credentials"}), 401
 
     token = jwt.encode({
-        'username': username,
-        'role': role,
+        'username': user.username,
+        'role': user.role,
         'exp': datetime.utcnow() + timedelta(hours=1)
     }, SECRET_KEY, algorithm="HS256")
 
