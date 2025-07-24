68f8c262a8cd10d3beaa4f9e257459fa4f6dca3e
Tudor Leonte <Tudor.Leonte@endava.com>
2025-07-24 13:26:48 +0300

    op

---
+++ app/logging/-- SQLite.sql
@@ -0,0 +1,4 @@
+-- SQLite
+select * from users;
+insert into users (username, password, email, role) values ('admin', 'admin123', 'admin@example.com', 'admin');
+insert into users (username, password, email, role) values ('user1', 'user123', 'user1@example.com', 'user');
